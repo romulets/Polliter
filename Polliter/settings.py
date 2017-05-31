3d9bed185e3faa973ede31819d7986eed24d80eb
Rômulo Farias <romulodefarias@gmail.com>
2017-05-31 19:45:19 -0300

    Update topics app
    on route topics/ getting only topics related (themes and politicians) to the logged user. If you want all topics ordered, call topics/all/

--- Polliter/settings.py
+++ Polliter/settings.py
@@ -36,8 +36,10 @@ LOGIN_REDIRECT_URL = '/home'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
--- topic/views.py
+++ topic/views.py
@@ -4,16 +4,35 @@ from __future__ import unicode_literals
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import permissions, status
+from django.core.paginator import Paginator
+from django.db.models import Q
 from topic.models import Topic, Vote
 from topic.serializers import TopicSerializer
 
 
 # Create your views here.
+@api_view(['GET'])
+def list_topics(request):
+    try:
+        page = int(request.GET.get('page', 1))
+        if page < 1:
+            page = 1
+    except ValueError:
+        page = 1
+
+    topics = Topic.objects.all().order_by('-date')
+    serializer = TopicSerializer(topics, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 @api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
-def list_topics(request):
-    topics = Topic.objects.all().order_by('-date')
+def list_topics_for_user(request):
+    user = request.user
+    topics = Topic.objects.filter(
+        Q(politicians__users__user=user) |
+        Q(themes__users__user=user)
+    ).order_by('-date').distinct()
     serializer = TopicSerializer(topics, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
