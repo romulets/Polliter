e4edfe2342dab31cca728b3b888b693b7dbb82e7
Rômulo Farias <romulodefarias@gmail.com>
2017-06-03 14:10:30 -0300

    Update main.js
    Update file's identation

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -6,104 +6,104 @@
 
 var app = angular.module('Polliter', []);
 
-app.config(function($interpolateProvider) {
+app.config(function ($interpolateProvider) {
   $interpolateProvider.startSymbol('[[');
   $interpolateProvider.endSymbol(']]');
 });
 
-app.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+app.config(['$httpProvider', function ($httpProvider) {
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
 
-app.controller('PrivateTopicsController', function($scope, $http) {
-	$scope.topics = [];
-	var page = 1;
+app.controller('PrivateTopicsController', function ($scope, $http) {
+  $scope.topics = [];
+  var page = 1;
 
   var getClients = function () {
-    $http.get('/topics?json=1&page=' + page).then(function(response) {
+    $http.get('/topics?json=1&page=' + page).then(function (response) {
       $scope.topics = response.data;
       page++;
-    }, function(err){
+    }, function (err) {
       console.error(err);
-      });
-	};
+    });
+  };
 
-	getClients();
+  getClients();
 });
 
-app.controller('PublicTopicsController', function($scope, $http) {
-	$scope.topics = [];
-	var page = 1;
+app.controller('PublicTopicsController', function ($scope, $http) {
+  $scope.topics = [];
+  var page = 1;
 
   var getClients = function () {
-    $http.get('/topics/all?json=1&page=' + page).then(function(response) {
+    $http.get('/topics/all?json=1&page=' + page).then(function (response) {
       $scope.topics = response.data;
       page++;
-    }, function(err){
+    }, function (err) {
       console.error(err);
-      });
-	};
+    });
+  };
 
-	getClients();
+  getClients();
 });
 
-app.controller('PoliticiansController', function($scope, $http) {
-	$scope.politicians = [];
+app.controller('PoliticiansController', function ($scope, $http) {
+  $scope.politicians = [];
 
   var getPoliticians = function () {
-    $http.get('/politicians/list?json=1').then(function(response) {
+    $http.get('/politicians/list?json=1').then(function (response) {
       $scope.politicians = response.data;
-    }, function(err){
-       console.error(err);
+    }, function (err) {
+      console.error(err);
     });
   };
 
   $scope.follow = function follow(politician, token) {
-    $http.post('/politicians/follow/' + politician.id +'/?json=1').then(function(response) {
+    $http.post('/politicians/follow/' + politician.id + '/?json=1').then(function (response) {
       politician.following = response.data.following;
-    }, function(err){
-       console.error(err);
+    }, function (err) {
+      console.error(err);
     });
   };
 
   $scope.unfollow = function unfollow(politician) {
-    $http.delete('/politicians/unfollow/' + politician.id +'/?json=1').then(function(response) {
+    $http.delete('/politicians/unfollow/' + politician.id + '/?json=1').then(function (response) {
       politician.following = response.data.following;
-    }, function(err){
-       console.error(err);
+    }, function (err) {
+      console.error(err);
     });
   };
 
-	getPoliticians()
+  getPoliticians()
 });
 
-app.controller('ThemesController', function($scope, $http) {
-	$scope.themes = [];
+app.controller('ThemesController', function ($scope, $http) {
+  $scope.themes = [];
 
   var getThemes = function () {
-    $http.get('/themes/list?json=1').then(function(response) {
+    $http.get('/themes/list?json=1').then(function (response) {
       $scope.themes = response.data;
-    }, function(err){
-       console.error(err);
+    }, function (err) {
+      console.error(err);
     });
   };
 
   $scope.follow = function follow(theme, token) {
-    $http.post('/themes/follow/' + theme.id +'/?json=1').then(function(response) {
+    $http.post('/themes/follow/' + theme.id + '/?json=1').then(function (response) {
       theme.following = response.data.following;
-    }, function(err){
-       console.error(err);
+    }, function (err) {
+      console.error(err);
     });
   };
 
   $scope.unfollow = function unfollow(theme) {
-    $http.delete('/themes/unfollow/' + theme.id +'/?json=1').then(function(response) {
+    $http.delete('/themes/unfollow/' + theme.id + '/?json=1').then(function (response) {
       theme.following = response.data.following;
-    }, function(err){
-       console.error(err);
+    }, function (err) {
+      console.error(err);
     });
   };
 
-	getThemes()
+  getThemes()
 });
